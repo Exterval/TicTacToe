9e0e6396466959f85c512f0731ebbc7d889a5901
Exterval <treblmao@gmail.com>
2026-01-12 22:56:36 +0800

    feat: update App component to handle symbol changes and improve button functionality

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -3,18 +3,26 @@ import './App.css'
 
 function App() {
   // tic-tac-toe app
-  const [symbol, setSymbol] = useState(['X','O']);
+  const [symbol, setSymbol] = useState('X');
 
   const buttonList = []
 
   for(let i = 0; i < 3; i++){
-    buttonList.push(<button className='square'></button>)
+    buttonList.push(<button className='square' onClick={(e)=>handleSymbolChange(e)}></button>)
+  }
+
+  function handleSymbolChange(e){
+    setSymbol(s => s === 'X' ? '0' : 'X');
+    e.target.textContent = symbol;
+    e.target.disabled = true;
+    return;
   }
 
   return (
     <>
       <div>
         <h1>Tic Tac Toe</h1>
+        <p>Next Player: {symbol[0]}</p>
         <div className="gameUI">
           <div className="rowOne">
             {buttonList}
