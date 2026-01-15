0239c944a2a145885f47141689150df7b5b552df
Exterval <treblmao@gmail.com>
2026-01-15 21:45:35 +0800

    feat: add diagonal match check in gridCheck function

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -35,6 +35,18 @@ function App() {
         console.log('Vertical Match')
       }
     }
+
+    // check column for diagonal match (0, 3, 8 & 2, 4, 6) * FIX
+    for(let i = 0; i < 3; i++){
+      console.log(arr[i] !== '', arr[i + 6], arr[i])
+      console.log(arr[i + 2], arr[i + 4], arr[i + 6])
+      if(arr[i + 2] === arr[i + 4] && arr[i+4] === arr[i + 6] && arr[i] !== ''){
+        console.log('Diagonal Match')
+      }
+    }
+    // arr[i] === arr[i + 4] && arr[i+4] === arr[i + 8]
+
+    
   }
 
   const buttonList = []
